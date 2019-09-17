aca1bd6eb2ee286b7790a979793e474b79dad39c
Diogo Barbosa Silva Sousa <diogo.barbosa@gm5.com.br>
2019-09-16 23:21:28 -0300

    Primeiro teste usando o Vue no projeto.

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Exemplo de Componente</div>
+
+                    <div class="card-body">
+                        Eu sou um exemplo de componente em Vue no Laravel.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
